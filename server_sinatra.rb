2e2b5408adababccc9b22dfaa1c67f883c0c9368
kilianeifler <kurteifler+github@gmail.com>
2016-06-14 17:06:58 +0200

    Final Ruby Server
    Ruby Server on Sinatra, not runnable, issuses binding the _class.rb and parse the string

--- server_sinatra.rb
+++ server_sinatra.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'json'		#just because uncertainty
-#require '_class.rb'	#working on binding _class.rb
+require './_class'	#working on binding ruml_class.rb
 
 
 use Rack::Logger    #need for console log
@@ -18,43 +18,12 @@ get '/' do
 # redirect '/public/index.html'   #same effect, but some browser don't accept the redirection
 end
 
-# post '/foo' do
-#   json :rubyCode		#incoming"rubyCode", not the string, there is no string
-#   request.accept		#no use of this actually
-#   request.query_string
-# end
 post '/foo' do
-	params[:code]
+	params[ &code]
+	x = _class.new			#try to send code to _class.rb
+	x.ClassDiagram(:code);
 	content_type :json
-  { :data => params[:code]}.to_json  #echo with the string
-end
+  {:data => params[x]}.to_json  #echo with the string
 
-# get "/" do
-#   t = %w[text/css text/html application/javascript]
-#   request.accept              # ['text/html', '*/*']
-#   request.accept? 'application/javascript'  # true
-#   request.preferred_type(t)   # 'text/html'
-#   request.body                # Request-Body des Client (siehe unten)
-#   request.scheme              # "http"
-#   request.script_name         # "/example"
-#   request.path_info           # "/foo"
-#   request.port                # 80 has to be 4711
-#   request.request_method      # "GET"
-#   request.query_string        # ""
-#   request.content_length      # Länge des request.body
-#   request.media_type          # Medientypus von request.body
-#   request.host                # "example.com"
-#   request.get?                # true (ähnliche Methoden für andere Verben)
-#   request.form_data?          # false
-#   request["editor"]			  # Wert von einem Parameter; [] ist die Kurzform für den params Hash
-#   request.referrer            # Der Referrer des Clients oder '/'
-#   request.user_agent          # User-Agent (verwendet in der :agent Bedingung)
-#   request.cookies             # Hash des Browser-Cookies
-#   request.xhr?                # Ist das hier ein Ajax-Request?
-#   request.url                 # "http://example.com/example/foo"
-#   request.path                # "/example/foo"
-#   request.ip                  # IP-Adresse des Clients
-#   request.secure?             # false (true wenn SSL)
-#   request.forwarded?          # true (Wenn es hinter einem Reverse-Proxy verwendet wird)
-#   request.env                 # vollständiger env-Hash von Rack übergeben
-# end
+end
+# development end, running out of time and lose one's head
